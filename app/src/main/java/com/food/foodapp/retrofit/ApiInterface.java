20c4f12430efd4506d75f014cda563fac3dd871f
rogiauvi <rogiauvi@gmail.com>
2021-02-19 10:58:32 +0700

    Commit One

---
+++ app/src/main/java/com/food/foodapp/retrofit/ApiInterface.java
@@ -0,0 +1,18 @@
+package com.food.foodapp.retrofit;
+
+import com.food.foodapp.model.FoodData;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiInterface {
+
+    @GET("fooddata.json")
+    Call<List<FoodData>> getAllData();
+
+
+    // lets make our model class of json data.
+
+}
